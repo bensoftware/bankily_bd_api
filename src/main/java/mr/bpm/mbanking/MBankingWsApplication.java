fe40fc0dcbda58b26a88e75bccc5bb30b7a6b2f4
bpm0317 <bpm0317@PC-MOBILE2.bpm.adds>
2018-10-08 17:28:33 +0100

    removing some comments

--- src/main/java/mr/bpm/mbanking/MBankingWsApplication.java
+++ src/main/java/mr/bpm/mbanking/MBankingWsApplication.java
@@ -8,11 +8,5 @@ public class MBankingWsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MBankingWsApplication.class, args);
-		//fqfqsf
-		///bismillah
-		///push 1
-		//push 2
-		//push from client 1
-		// push from client 2
-}
+	}
 }
