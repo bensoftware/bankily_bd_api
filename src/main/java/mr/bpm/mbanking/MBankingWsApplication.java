f1b33e35efe2d8dce77b00c345256037fc679162
unknown <bpm0319@PC-MOBILE3.bpm.adds>
2018-10-08 14:46:26 +0000

    init

---
+++ src/main/java/mr/bpm/mbanking/MBankingWsApplication.java
@@ -0,0 +1,12 @@
+package mr.bpm.mbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MBankingWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MBankingWsApplication.class, args);
+	}
+}
