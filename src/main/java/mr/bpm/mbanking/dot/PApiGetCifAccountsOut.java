8fb4cdd88ec8f4222a28d32fec9c28a088932187
bpm0317 <bpm0317@PC-MOBILE2.bpm.adds>
2018-10-09 09:51:33 +0100

    PApiGetCifAccountsCursor Cursor get cif accounts

--- src/main/java/mr/bpm/mbanking/dot/PApiGetCifAccountsOut.java
+++ src/main/java/mr/bpm/mbanking/dot/PApiGetCifAccountsOut.java
@@ -2,7 +2,10 @@ package mr.bpm.mbanking.dot;
 
 import java.math.BigDecimal;
 
+import java.util.List;
+
 public class PApiGetCifAccountsOut {
+	public List<PApiGetCifAccountsCursor> pCursor;
 	public BigDecimal OL_TRANSACTION_ID;
 	public BigDecimal OL_ERROR_CODE;   
 	public String OS_ERROR_DESC;
