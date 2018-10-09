ec33e36e5f5a5860dde77be0bf9cb4d50262f0a6
bpm0316 <bpm0316@PC-MOBILE1.bpm.adds>
2018-10-09 14:37:09 +0100

    all interface dao .. enjoy !

---
+++ src/main/java/mr/bpm/mbanking/dao/RetCheckBookReqDao.java
@@ -0,0 +1,9 @@
+package mr.bpm.mbanking.dao;
+
+import mr.bpm.mbanking.dot.PApiRetchqbookReqIn;
+import mr.bpm.mbanking.dot.PApiRetchqbookReqOut;
+
+public interface RetCheckBookReqDao {
+
+	PApiRetchqbookReqIn getCheckNni(PApiRetchqbookReqOut in);
+}
