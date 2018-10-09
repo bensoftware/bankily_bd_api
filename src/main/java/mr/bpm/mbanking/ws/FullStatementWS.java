3943f8ef23ea53efa64f00a09aa5999955a46444
bpm0317 <bpm0317@PC-MOBILE2.bpm.adds>
2018-10-09 10:08:35 +0100

    Fullstatement interface first method

--- src/main/java/mr/bpm/mbanking/ws/FullStatementWS.java
+++ src/main/java/mr/bpm/mbanking/ws/FullStatementWS.java
@@ -1,5 +1,17 @@
 package mr.bpm.mbanking.ws;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+import mr.bpm.mbanking.dot.FullStatementIn;
+import mr.bpm.mbanking.dot.FullStatementOut;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+
 public interface FullStatementWS {
 
+	@WebMethod
+	FullStatementOut pApiFullStatement(FullStatementIn in);
 }
