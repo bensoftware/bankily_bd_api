5e0aae113061ef9bbcf0dd24b8b8edb73f643794
bpm0317 <bpm0317@PC-MOBILE2.bpm.adds>
2018-10-09 09:00:31 +0100

    PApiCreateCif In and Out date parameters

---
+++ src/main/java/mr/bpm/mbanking/dot/PApiCreateCifOut.java
@@ -0,0 +1,9 @@
+package mr.bpm.mbanking.dot;
+
+import java.math.BigDecimal;
+
+public class PApiCreateCifOut {
+	public BigDecimal OL_CIF_NO;
+	public BigDecimal OL_ERROR_CODE;
+	public String OS_ERROR_DESC; 
+}
