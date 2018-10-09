bab269e0b5b89cc1455ab896a796237e69a4082b
bpm0317 <bpm0317@PC-MOBILE2.bpm.adds>
2018-10-09 09:12:40 +0100

    PApiTransferEx3 In and Out Parameters

---
+++ src/main/java/mr/bpm/mbanking/dot/PApiTransferEx3Out.java
@@ -0,0 +1,12 @@
+package mr.bpm.mbanking.dot;
+
+import java.math.BigDecimal;
+
+public class PApiTransferEx3Out {
+	public BigDecimal OL_SHARE_REF;
+	public BigDecimal ODEC_AVAIL;
+	public BigDecimal OL_TRX_CODE;
+	public BigDecimal OL_TRANSACTION_ID;
+	public BigDecimal OL_ERROR_CODE;
+	public String OS_MESSAGE;
+}
