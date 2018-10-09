4af7ef9b482dc48c541e425177cd3f23cca74d81
bpm0317 <bpm0317@PC-MOBILE2.bpm.adds>
2018-10-09 09:04:17 +0100

    PApiCheckNni In and Out Parameters

---
+++ src/main/java/mr/bpm/mbanking/dot/PApiCheckNniOut.java
@@ -0,0 +1,9 @@
+package mr.bpm.mbanking.dot;
+
+import java.math.BigDecimal;
+
+public class PApiCheckNniOut {
+	public BigDecimal OL_TRANSACTION_ID;
+	public BigDecimal OL_ERROR_CODE;
+	public String OS_ERROR_DESC; 
+}
