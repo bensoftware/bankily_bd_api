38fa11c43bd1525cf01bac76f43bb78c59c3ee0e
cherif <moulaye.eli1990@gmail.com>
2019-09-27 09:27:00 +0000

    commit

--- src/main/java/mr/bpm/mbanking/SpringJDBCConfiguration.java
+++ src/main/java/mr/bpm/mbanking/SpringJDBCConfiguration.java
@@ -3,7 +3,6 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
  
 @Configuration
@@ -12,7 +11,6 @@ public class SpringJDBCConfiguration {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         //MySQL database we are using
- 
         dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
         dataSource.setUrl("jdbc:oracle:thin:@30.30.1.90:1528:monprod");//change url
         dataSource.setUsername("mxp");//change userid
